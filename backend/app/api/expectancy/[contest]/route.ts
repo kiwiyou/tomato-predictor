f5db153f9800ed0b36eef15c449bebeeb6b98e17
kiwiyou <kiwiyou@kiwiyou.dev>
2024-02-27 05:18:19 +0900

    Test stream

--- backend/app/api/expectancy/[contest]/route.ts
+++ backend/app/api/expectancy/[contest]/route.ts
@@ -25,48 +25,6 @@ export async function GET(
     const readable = new ReadableStream({
       async start(controller) {
         controller.enqueue(`[`);
-        let first = true;
-        try {
-          const E = await Promise.all(
-            handles.map(async (handle) => {
-              let numer = 0;
-              let denom = 0;
-              let i = 1;
-              for (const {
-                performance,
-                startTime,
-                ratedRangeEnd,
-              } of await getContests(handle)) {
-                const Ti = parseISO(startTime);
-                if (!isBefore(Ti, C)) continue;
-                const pi = unfix(performance, ratedRangeEnd);
-                const Wi = Math.min(
-                  Math.pow(0.8, i),
-                  Math.pow(0.25, Math.floor(differenceInDays(C, Ti) / 365)),
-                );
-                numer += pi * Wi;
-                denom += Wi;
-                i++;
-              }
-              let P;
-              if (denom === 0) {
-                const { rating } = await fetch(
-                  `https://solved.ac/api/v3/user/show?handle=${handle}`,
-                ).then((r) => r.json());
-                P = 800 + Math.floor(rating / 2.4);
-              } else {
-                P = numer / denom;
-              }
-              if (!first) {
-                controller.enqueue(`,`);
-              }
-              first = false;
-              controller.enqueue(`${P}`);
-              return P;
-            }),
-          );
-          redis.set(`E.${params.contest}`, E);
-        } catch {}
         controller.enqueue("]");
         controller.close();
       },
