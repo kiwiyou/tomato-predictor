aa2b96f45a24e6bb7b48065898fb18e52cd80626
kiwiyou <kiwiyou@kiwiyou.dev>
2024-02-27 06:26:45 +0900

    Revert to async

--- backend/app/api/expectancy/[contest]/route.ts
+++ backend/app/api/expectancy/[contest]/route.ts
@@ -22,66 +22,40 @@ export async function GET(
     }
     const C = parseISO(arena.startTime);
     const handles = await getContestants(arena.arenaId);
-    let first = true;
-    const iterator = getExpectancy(C, handles);
-    const E: number[] = [];
-    const readable = new ReadableStream({
-      start(controller) {
-        controller.enqueue("[");
-      },
-      async pull(controller) {
-        if (!first) {
-          controller.enqueue(",");
+    E = await Promise.all(
+      handles.map(async (handle) => {
+        let numer = 0;
+        let denom = 0;
+        let i = 1;
+        for (const {
+          performance,
+          startTime,
+          ratedRangeEnd,
+        } of await getContests(handle)) {
+          const Ti = parseISO(startTime);
+          if (!isBefore(Ti, C)) continue;
+          const pi = unfix(performance, ratedRangeEnd);
+          const Wi = Math.min(
+            0.8 ** i,
+            0.25 ** (differenceInDays(C, Ti) / 365)
+          );
+          numer += pi * Wi;
+          denom += Wi;
+          i++;
         }
-        first = false;
-        const next = await iterator.next();
-        if (next.done) {
-          controller.enqueue("]");
-          controller.close();
-          await redis.set(`E.${params.contest}`, E);
+        let P: number;
+        if (denom === 0) {
+          const { rating } = await fetch(
+            `https://solved.ac/api/v3/user/show?handle=${handle}`
+          ).then((r) => r.json());
+          P = 800 + Math.floor(rating / 2.4);
         } else {
-          controller.enqueue(next.value.toString());
-          E.push(next.value);
+          P = numer / denom;
         }
-      },
-    });
-    return new Response(readable, {
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-    });
+        return P;
+      })
+    );
+    await redis.set(`E.${params.contest}`, E);
   }
   return Response.json(E);
-}
-
-async function* getExpectancy(C: Date, handles: string[]) {
-  for (const handle of handles) {
-    let numer = 0;
-    let denom = 0;
-    let i = 1;
-    for (const { performance, startTime, ratedRangeEnd } of await getContests(
-      handle
-    )) {
-      const Ti = parseISO(startTime);
-      if (!isBefore(Ti, C)) continue;
-      const pi = unfix(performance, ratedRangeEnd);
-      const Wi = Math.min(
-        Math.pow(0.8, i),
-        Math.pow(0.25, Math.floor(differenceInDays(C, Ti) / 365))
-      );
-      numer += pi * Wi;
-      denom += Wi;
-      i++;
-    }
-    let P;
-    if (denom === 0) {
-      const { rating } = await fetch(
-        `https://solved.ac/api/v3/user/show?handle=${handle}`
-      ).then((r) => r.json());
-      P = 800 + Math.floor(rating / 2.4);
-    } else {
-      P = numer / denom;
-    }
-    yield P;
-  }
 }
